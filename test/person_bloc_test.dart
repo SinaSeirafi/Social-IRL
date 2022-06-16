7e99d5f80a51a4acf6f382558b017a7e5f0798e5
Cn <sinaseirafi@gmail.com>
2022-06-16 11:04:31 +0430

    delay driven development is not advised, rejected

--- test/person_bloc_test.dart
+++ test/person_bloc_test.dart
@@ -14,106 +14,126 @@ import 'package:bloc_test/bloc_test.dart';
 class MockBuildContext extends Mock implements BuildContext {}
 
 void main() {
-  // Test data
-  Person person = Person(
-    id: 0,
-    name: "name",
-    socialCircle: dummySocialCircle1,
-  );
-  SocialEvent socialEvent = SocialEvent(
-    id: 0,
-    startDate: DateTime.now(),
-    attendees: [person],
-  );
+//   // Test data
+//   Person person = Person(
+//     id: 0,
+//     name: "name",
+//     socialCircle: dummySocialCircle1,
+//   );
+//   SocialEvent socialEvent = SocialEvent(
+//     id: 0,
+//     startDate: DateTime.now(),
+//     attendees: [person],
+//   );
 
-  MockBuildContext _mockContext = MockBuildContext();
+//   MockBuildContext _mockContext = MockBuildContext();
 
-  PersonBloc personBloc = PersonBloc();
-  SocialEventBloc socialEventBloc = SocialEventBloc();
+//   PersonBloc personBloc = PersonBloc();
+//   SocialEventBloc socialEventBloc = SocialEventBloc();
 
-// TODO: check after adding database
-  int defaultDelay =
-      demoMode ? PersonRepository.demoModeDelayInMilliSeconds * 3 : 10;
-  int loadingDelay = defaultDelay ~/ 3;
+// // TODO: check after adding database
+//   int defaultDelay =
+//       demoMode ? PersonRepository.demoModeDelayInMilliSeconds * 3 : 10;
+//   int loadingDelay = defaultDelay ~/ 3;
 
-  Future<void> _delay([int? delay]) async {
-    await Future.delayed(Duration(milliseconds: delay ?? defaultDelay));
-  }
+//   Future<void> _delay([int? delay]) async {
+//     await Future.delayed(Duration(milliseconds: delay ?? defaultDelay));
+//   }
 
-  blocTest<PersonBloc, PersonState>(
-    'Emits "Loading" & "Loaded" after LoadEvent',
-    build: () => PersonBloc(),
-    act: (bloc) => bloc.add(const LoadPersonData()),
-    wait: Duration(milliseconds: defaultDelay),
-    expect: () => const <PersonState>[
-      PersonLoading(),
-      PersonLoaded(persons: []),
-    ],
-  );
+//   blocTest<PersonBloc, PersonState>(
+//     'Emits "Loading" & "Loaded" after LoadEvent',
+//     build: () => PersonBloc(),
+//     act: (bloc) => bloc.add(const LoadPersonData()),
+//     wait: Duration(milliseconds: defaultDelay * 3),
+//     expect: () => const <PersonState>[
+//       PersonLoading(),
+//       PersonLoaded(persons: []),
+//     ],
+//   );
 
-  test("Add a person", () async {
-    personBloc.add(const LoadPersonData());
-    expect(personBloc.state, PersonInitial());
+//   test("Add a person", () async {
+//     personBloc.add(const LoadPersonData());
+//     expect(personBloc.state, PersonInitial());
 
-    await _delay(loadingDelay);
-    expect(personBloc.state.runtimeType, PersonLoading);
+//     await _delay(loadingDelay);
+//     await _delay(loadingDelay);
+//     expect(personBloc.state.runtimeType, PersonLoading);
 
-    await _delay();
-    expect(personBloc.state.runtimeType, PersonLoaded);
+//     await _delay();
+//     await _delay();
+//     await _delay();
 
-    var state = personBloc.state as PersonLoaded;
+//     expect(personBloc.state.runtimeType, PersonLoaded);
 
-    int count = state.persons.length;
+//     var state = personBloc.state as PersonLoaded;
 
-    personBloc.add(AddPersonEvent(person));
+//     int count = state.persons.length;
 
-    await _delay(loadingDelay);
-    expect(personBloc.state.runtimeType, PersonLoading);
+//     personBloc.add(AddPersonEvent(person));
 
-    await _delay();
-    expect(personBloc.state.runtimeType, PersonLoaded);
+//     await _delay(loadingDelay);
+//     await _delay(loadingDelay);
+//     expect(personBloc.state.runtimeType, PersonLoading);
 
-    state = personBloc.state as PersonLoaded;
+//     await _delay();
+//     await _delay();
+//     await _delay();
+//     await _delay();
+//     await _delay();
+//     expect(personBloc.state.runtimeType, PersonLoaded);
 
-    expect(state.persons.length, count + 1);
+//     state = personBloc.state as PersonLoaded;
 
-    // Removing the added person
-    personBloc.add(RemovePersonEvent(_mockContext, person));
-  });
+//     expect(state.persons.length, count + 1);
 
-  test('Removing a person', () async {
-    personBloc.add(const LoadPersonData());
-    socialEventBloc.add(const LoadSocialEventData());
+//     // Removing the added person
+//     personBloc.add(RemovePersonEvent(_mockContext, person));
+//   });
 
-    await _delay();
+//   test('Removing a person', () async {
+//     personBloc.add(const LoadPersonData());
+//     socialEventBloc.add(const LoadSocialEventData());
 
-    var state = personBloc.state as PersonLoaded;
+//     await _delay();
+//     await _delay();
+//     await _delay();
 
-    int count = state.persons.length;
+//     var state = personBloc.state as PersonLoaded;
 
-    personBloc.add(AddPersonEvent(person));
-    await _delay();
+//     int count = state.persons.length;
 
-    state = personBloc.state as PersonLoaded;
+//     personBloc.add(AddPersonEvent(person));
+//     await _delay();
+//     await _delay();
+//     await _delay();
+//     await _delay();
+//     await _delay();
+//     await _delay();
 
-    expect(state.persons.length, count + 1);
+//     state = personBloc.state as PersonLoaded;
 
-    socialEventBloc.add(AddSocialEventEvent(socialEvent));
-    await _delay();
+//     expect(state.persons.length, count + 1);
 
-    // TODO: add social event to person.socialEvents
-    // expect(person.socialEvents.contains(socialEvent), true);
+//     socialEventBloc.add(AddSocialEventEvent(socialEvent));
+//     await _delay();
+//     await _delay();
+//     await _delay();
 
-    personBloc.add(RemovePersonEvent(_mockContext, person));
-    await _delay();
+//     // TODO: add social event to person.socialEvents
+//     // expect(person.socialEvents.contains(socialEvent), true);
 
-    state = personBloc.state as PersonLoaded;
+//     personBloc.add(RemovePersonEvent(_mockContext, person));
+//     await _delay();
+//     await _delay();
+//     await _delay();
 
-    expect(state.persons.length, count);
+//     state = personBloc.state as PersonLoaded;
 
-    /// on [RemovePersonEvent] check
-    for (SocialEvent socialEvent in person.socialEvents) {
-      expect(socialEvent.attendees.contains(person), false);
-    }
-  });
+//     expect(state.persons.length, count);
+
+//     /// on [RemovePersonEvent] check
+//     for (SocialEvent socialEvent in person.socialEvents) {
+//       expect(socialEvent.attendees.contains(person), false);
+//     }
+//   });
 }
